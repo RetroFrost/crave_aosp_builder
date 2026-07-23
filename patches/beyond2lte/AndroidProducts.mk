274ad0eb5c4501bfa264190fb95c837374dc2294
RetroFrost <routerifter@gmail.com>
2026-07-22 22:37:31 -0300

    Add PixelOS 15 beyond2lte lunch target override

---
+++ patches/beyond2lte/AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# PixelOS 15 lunch targets for Samsung Galaxy S10+ (beyond2lte)
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_beyond2lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_beyond2lte-bp1a-eng \
+    aosp_beyond2lte-bp1a-user \
+    aosp_beyond2lte-bp1a-userdebug
